7411727b4c89c9a1afa02e7153c76f77053a0e31
Hasan10100 <e20131@eng.pdn.ac.lk>
2026-03-23 07:17:26 +0530

    Update URL endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = 'http://127.0.0.1:8000/';
 
 interface Seat {
   id: number;
